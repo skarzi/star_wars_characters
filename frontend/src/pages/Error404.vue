d689d504df94c98d55d1e6aef7abb4e6e1ad9ba3
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2020-09-02 12:26:38 +0200

    adjust 404 page to star wars theme

--- frontend/src/pages/Error404.vue
+++ frontend/src/pages/Error404.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-black text-warning text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
         404
@@ -11,8 +11,8 @@
 
       <q-btn
         class="q-mt-xl"
-        color="white"
-        text-color="blue"
+        color="warning"
+        text-color="black"
         unelevated
         to="/"
         label="Go Home"
